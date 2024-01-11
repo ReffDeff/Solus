89fef63badf00e678c48e633fdf31843c129c93d
ReffDeff <dan.alexsandrovpro@gmail.com>
2024-01-11 21:08:16 +0300

    Still working

--- Source/Solus/Public/Items/ItemBase.h
+++ Source/Solus/Public/Items/ItemBase.h
@@ -3,15 +3,53 @@
 #pragma once
 
 #include "CoreMinimal.h"
-#include "UObject/NoExportTypes.h"
+#include "Data/ItemDataStructs.h"
 #include "ItemBase.generated.h"
 
-/**
- * 
- */
+
 UCLASS()
 class SOLUS_API UItemBase : public UObject
 {
 	GENERATED_BODY()
+
+public:
+	//===================================================
+	// PROPERTIES & VARIABLES
+	//===================================================
 	
+	UPROPERTY(VisibleAnywhere, Category= "Item Data", meta = (UIMin = 1, UIMax = 100))
+	int32 Quantity;
+	
+	UPROPERTY(EditAnywhere, Category= "Item Data")
+	FName ID;
+
+	UPROPERTY(EditAnywhere, Category= "Item Data")
+	EItemType ItemType;
+
+	UPROPERTY(EditAnywhere, Category= "Item Data")
+	EItemQuality ItemQuality;
+
+	UPROPERTY(EditAnywhere, Category= "Item Data")
+	FItemStatistics ItemStatistics;
+
+	UPROPERTY(EditAnywhere, Category= "Item Data")
+	FItemTextData TextData;
+
+	UPROPERTY(EditAnywhere, Category= "Item Data")
+	FItemNumericData NumericData;
+	
+	UPROPERTY(EditAnywhere, Category= "Item Data")
+	FItemAssetData AssetData;
+
+	//===================================================
+	// FUNCTIONS
+	//===================================================
+
+	UItemBase();
+
+	UItemBase* CreateItemCopy();
+
+	FORCEINLINE float GetItemStackWeight() const {return Quantity * NumericData.Weight;};
+	
+protected:	
 };
